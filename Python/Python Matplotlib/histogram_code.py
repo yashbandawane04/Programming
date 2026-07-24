a259d4f4aced64cfe3888a8bb8b58bf3245a03ff
Yash Bandawane <yashbandawane04@gmail.com>
2026-07-24 12:03:51 +0530

    Delete Python/Python Matplotlib/histogram_code.py

--- Python/Python Matplotlib/histogram_code.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-data = [12,45,67,23,45,67,89,45,67,12]
-plt.figure(figsize=(8,4))
-plt.hist(data, bins=10)
-plt.title("Histogram of Data")
-plt.xlabel("Value")
-plt.ylabel("Frequency")
-plt.show()
